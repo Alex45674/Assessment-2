8f2587ee5b9b87fddc06f3e9f4098cc341b001de
C1027505 <A.J.Portess2@newcastle.ac.uk>
2021-11-08 18:53:00 +0000

    Having several issues. When trying to run my interface it runs all my python files and when trying to input any contact data i get an error.

--- options/add_contact.py
+++ options/add_contact.py
@@ -3,3 +3,6 @@ name = input("Enter name of the contact being added: ")
 address = input("Enter the address: ")
 phoneNo = input("Enter their phone number:")
 birthday = input("Enter their birthday in the form ##/##/####: ")
+
+with open("contact_data.csv","w") as data:
+    data.write(name,address,phoneNo,birthday)

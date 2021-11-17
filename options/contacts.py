20069c15b4d86d41d3203f9ef535a775ec146b6b
C1027505 <A.J.Portess2@newcastle.ac.uk>
2021-11-17 15:58:30 +0000

    Fixed and finished my search for a contact option. You are able to input either a name, address, phone number and birthday and the code will search through the contacts till a contact that has a matching string is found and that contact will be displayed. If not found a different message will be shown.

--- options/contacts.py
+++ options/contacts.py
@@ -2,13 +2,13 @@ import csv
 # Executes a section of code that allows the user to add a new contact to the contact data.
 def add_contact():
 
-    print("You have selected option 1 (Add a contact)")
+    print("You have selected option 1 (Add a contact):")
     print('')
 
     # Asks the user for the details required to add a new contact.
     name = input("Enter name of the contact being added: ")
     address = input("Enter the address: ")
-    phoneno = input(str("Enter their phone number in the form: "))
+    phoneno = input(str("Enter their phone number: "))
     birthday = input("Enter their birthday in the form ##/##/####: ")
 
     # Puts all the new contacts data together to be written as a new row.
@@ -26,7 +26,7 @@ def add_contact():
 # Executes a section of code that allows the user to view all contacts stored in the contacts data file.
 def all_contacts():
 
-    print("You have selected option 2 (Show all contacts)")
+    print("You have selected option 2 (Show all contacts):")
     print('')
 
     # This opens the contact data for reading
@@ -35,15 +35,14 @@ def all_contacts():
         for line in data:
             print(line)
 
-    print('')
     print("All contacts shown.")
 
 # Executes a section of code that allows the user to find a contact by entering a name,address,phone number or birthday.
 def find_contact():
 
-    print("You have selected option 3 (Find a contact)")
+    print("You have selected option 3 (Find a contact):")
     print('')
-    # Creates an empty list to store the contacts information
+    # Creates an empty list to store the contacts information.
     contacts_list = []
     # Opens the file contact data
     with open("contact_data.csv", newline='') as data:
@@ -55,15 +54,31 @@ def find_contact():
         # Asks the user to enter one of the
         search_info = str(input("""Enter the name, address, phone number or birthday of the contact you want to find
 (Note when entering a birthday please enter it in the form ##/##/####): """))
+        print('')
 
-        row_num = 0
-        # Goes through each row in my contacts_list
+        # Tells me the number of rows present in the contact data file.
+        list_length = len(contacts_list)
+
+        # This is used to keep track of what row the element is located.
+        row_num = -1
+
+        # Goes through each row in my contacts_list.
         for row in contacts_list:
+            # Adds one to the row number to show it's starting to search a new row.
+            row_num = row_num + 1
             for information in row:
-                row_num = row_num + 1
+                # Checks if any of the contact infromation in that row is the same as the inputted search information.
                 if information == search_info:
                     print("Contact found: ", contacts_list[row_num])
+                    # Breaks the loop
+                    break
+                # If the desired information isn;t found and the loop has gone through all rows this code will be ran.
+                elif (row_num + 1) == list_length:
+                    print('Contact does not exist.')
+                    # This stops the print statement being repeated several times
                     break
                 else:
+                    # Allows the loop to continue
                     continue
+
 

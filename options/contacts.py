73aa8c8b1ac9f1e42c4b7fa9a5039c6544633aaf
C1027505 <A.J.Portess2@newcastle.ac.uk>
2021-11-17 21:49:41 +0000

    Changed the find a contact option so it prints all the contacts that have the information being searched for. Also worked more on my edit contact option so that it searches through the contacts but asks the user each time it finds a contact that contains the same information as the inputted information if that is the contact they want to edit if not it continues through the contacts and does the same thing if another contact is found. Else it says the contact there looking for doesn't exist.

--- options/contacts.py
+++ options/contacts.py
@@ -1,24 +1,25 @@
 import csv
+from options import contacts_cli
 # Executes a section of code that allows the user to add a new contact to the contact data.
 def add_contact():
 
     print("You have selected option 1 (Add a contact):")
     print('')
 
-    # Asks the user for the details required to add a new contact.
+# Asks the user for the details required to add a new contact.
     name = input("Enter the full name (first and last names) of the contact being added: ")
     address = input("Enter their street address: ")
     phoneno = input(str("Enter their phone number: "))
     birthday = input("Enter their birthday in the form ##/##/####: ")
 
-    # Puts all the new contacts data together to be written as a new row.
+# Puts all the new contacts data together to be written as a new row.
     new_contact = (name, address, phoneno, birthday)
 
-    # Opens the contact data file in a way that new data can be added onto the end of the contact data file.
+# Opens the contact data file in a way that new data can be added onto the end of the contact data file.
     with open("contact_data.csv", "a", newline='') as data:
         write_contact = csv.writer(data)
 
-        # Writes the new contacts information in a row.
+# Writes the new contacts information in a row.
         write_contact.writerow(new_contact)
     print('')
     print("New contact added.")
@@ -29,7 +30,7 @@ def all_contacts():
     print("You have selected option 2 (Show all contacts):")
     print('')
 
-    # This opens the contact data for reading
+# This opens the contact data for reading
     with open("contact_data.csv", "r") as data:
         # This print each line in the csv file
         for line in data:
@@ -42,62 +43,93 @@ def find_contact():
 
     print("You have selected option 3 (Find a contact):")
     print('')
-    # Creates an empty list to store the contacts information.
+# Creates an empty list to store the contacts information.
     contacts_list = []
-    # Opens the file contact data
+# Opens the file contact data
     with open("contact_data.csv", newline = '') as data:
         read_data = csv.reader(data, delimiter = ',')
-        # For each row in contact data, it is added on to the end of my list (contact_list)
+# For each row in contact data, it is added on to the end of my list (contact_list)
         for row in read_data:
             contacts_list.append(row)
 
-    # Asks the user to enter one bit of information about the contact their trying to find.
+# Asks the user to enter one bit of information about the contact their trying to find.
     search_info = str(input("""Enter the full name (first and Last), street address, phone number or birthday of the
 contact you want to find. (Note when entering a birthday please enter it in the form ##/##/####): """))
     print('')
-
-    # Tells me the number of rows present in the contact data file.
-    list_length = len(contacts_list)
-
-    # This is used to keep track of what row the element is located.
+# This is used to keep track of what row the information is located.
     row_num = -1
-
-    # Goes through each row in my contacts_list.
+# This is used to check a contacts exists.
+    check = False
+# Goes through each row in my contacts_list.
     for row in contacts_list:
-        # Adds one to the row number to show it's starting to search a new row.
+# Adds one to the row number to show it's starting to search a new row.
         row_num = row_num + 1
         for information in row:
-            # Checks if any of the contact infromation in that row is the same as the inputted search information.
+# Checks if any of the contact information in that row is the same as the inputted search information.
             if information == search_info:
-                print("Contact found: ", contacts_list[row_num])
-                # Breaks the loop
-                break
-                # If the desired information isn;t found and the loop has gone through all rows this code will be ran.
-            elif (row_num + 1) == list_length:
-                print('Contact does not exist.')
-                # This stops the print statement being repeated several times
-                break
+                print("Contact(s) found: ", contacts_list[row_num])
+# Check is set to true to we know the contact exists.
+                check = True
+# If the desired information isn;t found and the loop has gone through all rows this code will be ran.
             else:
-                # Allows the loop to continue
+# Allows the loop to continue
                 continue
+# Runs if contact doesn't exist.
+    if check == False:
+        print('Contact does not exist.')
 
 def edit_contact():
 
     print('You have selected option 4 (edit a contact)')
     print('')
 
-    # Again creates an empty list.
+# Again creates an empty list.
     contacts_list = []
-    # Opens the file contact data.
+
+# Opens the file contact data.
     with open("contact_data.csv", newline = '') as data:
         read_data = csv.reader(data, delimiter = ',')
-        # For each row in contact data, it is added on to the end of my list (contact_list).
+
+# For each row in contact data, it is added on to the end of my list (contact_list).
         for row in read_data:
             contacts_list.append(row)
 
-    # Asks the user to enter one bit of information of the contact their trying to edit so the contact can be located.
+# Asks the user to enter one bit of information of the contact their trying to edit so the contact can be located.
     search_info = str(input("""Enter the full name (first and Last), street address, phone number or birthday of the
 contact you want to edit. (Note when entering a birthday please enter it in the form ##/##/####): """))
 
-    # Tells me the number of rows present in the contacts_list.
-    rows_present = len(contacts_list)
+# This is used to keep track of what row the information wanting to be edited is located.
+    row_num = -1
+# This is used to check a contacts existence.
+    check = False
+# Goes through each row in my contacts_list.
+    for row in contacts_list:
+# Adds one to the row number to show it's starting to search a new row.
+        row_num = row_num + 1
+        for information in row:
+# Checks if any of the contact infromation in that row is the same as the inputted search information.
+            if information == search_info:
+                print('')
+# Shows the user the first contact to be found.
+                print("Contact(s) found: ", contacts_list[row_num])
+# Check is set to true so we know the contact exists.
+                check = True
+# Asks the user if this is the correct contact.
+                search = str(input("""
+Is this the contact you wanted to edit?
+Please enter 'yes' if it is or 'no' to keep searching: """))
+# Depending on the users input
+                if search == 'yes':
+
+                    break
+                elif search == 'no':
+                    check = False
+                    break
+            else:
+# Allows the loop to continue.
+                continue
+# Runs if the contact doesn't exist.
+    if check == False:
+        print('')
+        print('Contact does not exist.')
+
--- options/contacts_cli.py
+++ options/contacts_cli.py
@@ -27,3 +27,25 @@ Enter the number of the option you would like to perform: """)
         quit()
     else:
         print("Sorry that is not one of the options try again.")
+
+def edit_options():
+    edit = str(input("""What would you like to do:
+1) Change name
+2) Change address
+3) Change phone number
+4) Change birthday
+5) Delete the contact
+
+Enter the option you would like to perform: """))
+    if edit == '1':
+        contacts.change_name()
+    elif edit == '2':
+        contacts.change_address()
+    elif edit === '3':
+        contacts.change_phonenum()
+    elif edit == '4':
+        contacts.change_birthday()
+    elif edit == '5':
+        contacts.delete()
+    else:
+        print("Sorry that wasn't an option")
